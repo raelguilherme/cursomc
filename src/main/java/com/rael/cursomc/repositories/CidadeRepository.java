6b813f15e59dc481e3cb7d294873e2edb6ec874b
Rael <raelguilherme@gmail.com>
2018-11-02 09:03:14 -0300

    Estado e Cidade

---
+++ src/main/java/com/rael/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.rael.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rael.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer>{
+
+}
